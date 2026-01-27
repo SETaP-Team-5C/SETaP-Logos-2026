3887b7d4bc7fc22d610fbc8f276087dfc2ac8670
Mattcodez21 <166388565+Mattcodez21@users.noreply.github.com>
2026-01-27 17:37:08 +0000

    Draw Top Right Shield

--- import turtle.py
+++ import turtle.py
@@ -7,15 +7,22 @@ def mid_top():
     t.forward(200)
     t.pendown()
 
-def left_sheild():
+def left_shield():
     mid_top()
     t.left(215)
     t.circle(150, 90)
 
+def right_shield():
+    t.penup()
+    mid_top()
+    t.right(215)
+    t.pendown()
+    t.circle(-150, 90)
+
 
 def sheild():
-    left_sheild()
-    #right_sheild()
+    left_shield()
+    right_shield()
 
 
 
